577479929ecf00f61bb82a5ba2354718ae841550
Nikita Kirakosyan <nikita.kirakosyan.work@gmail.com>
2025-04-24 20:43:52 +0300

    ReadOnlyAttribute added

--- Assets/handy-editor-extensions/Scripts/DemoScripts/PlayerDemo.cs
+++ Assets/handy-editor-extensions/Scripts/DemoScripts/PlayerDemo.cs
@@ -4,10 +4,12 @@ namespace HandyEditorExtensions
 {
     public class PlayerDemo : MonoBehaviour
     {
-        [Max(100)]public int health = 100;
-        [Max(nameof(maxHealth))]public int health2 = 100;
+        [Max(100)] public int health = 100;
+        [Max(nameof(maxHealth))] public int health2 = 100;
         public int maxHealth = 200;
         public float damage = 50;
         public float damageMultiplier = 0.5f;
+        [ReadOnly] public string readOnlyStringField = "read only string field";
+        [ReadOnly] public Vector3 readOnlyVector3Field = new (1, 2, 3);
     }
 }
--- Assets/handy-editor-extensions/Scripts/Editor/PropertyDrawers/Attributes/MaxAttributePropertyDrawer.cs
+++ Assets/handy-editor-extensions/Scripts/Editor/PropertyDrawers/Attributes/MaxAttributePropertyDrawer.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace HandyEditorExtensions
+namespace HandyEditorExtensions.Editor
 {
     [CustomPropertyDrawer(typeof(MaxAttribute))]
     public class MaxAttributePropertyDrawer : PropertyDrawer
