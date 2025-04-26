a7b3602fd70ab2e625a4f576a0eac6b50ea7f800
Nikita Kirakosyan <nikita.kirakosyan.work@gmail.com>
2025-04-26 20:02:21 +0300

    ProgressBarAttributePropertyDrawer updated

--- Assets/handy-editor-extensions/Editor/Scripts/PropertyDrawers/ProgressBarAttributePropertyDrawer.cs
+++ Assets/handy-editor-extensions/Editor/Scripts/PropertyDrawers/ProgressBarAttributePropertyDrawer.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 
-namespace HandyEditorExtensions
+namespace HandyEditorExtensions.Editor
 {
     [CustomPropertyDrawer(typeof(ProgressBarAttribute))]
     public class ProgressBarAttributePropertyDrawer : PropertyDrawer
